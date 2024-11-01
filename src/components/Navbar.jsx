fa33fadc6eaf09d0c75b5949f0df70d95e37c5fa
Shahid 10 <shelbyarthur874@gmail.com>
2024-11-01 10:22:05 +0530

    "nav animations added"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import React, { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-
   const [scrolled, setScrolled] = useState(false);
   const menuRef = useRef();
   useEffect(() => {
@@ -23,11 +22,22 @@ const Navbar = () => {
     };
   }, []);
 
+  // const menuTl = gsap.timeline();
+
   useGSAP(() => {
     if (menuOpen) {
       gsap.from(menuRef.current, {
         opacity: 0,
         duration: 0.1,
+      });
+
+      const q = gsap.utils.selector(menuRef);
+      gsap.from(q("h1"), {
+        opacity: 0,
+        y: 20,
+        duration: 0.5,
+        stagger: 0.3,
+        // ease: "power2.out",
       });
     }
   }, [menuOpen]);
@@ -79,25 +89,34 @@ const Navbar = () => {
         ref={menuRef}
         className={
           menuOpen
-            ? "full h-fit bg-black/80 shadow-lg shadow-white/90 w-[60%] md:w-[30%] absolute right-[2.5rem] top-[4rem] transition-all duration-300 ease-in-out flex flex-col  justify-center items-center gap-[2rem] px-[2.5rem] py-[1rem] text-3xl overflow-hidden rounded-lg font-[font-2] "
+            ? "full h-fit bg-yellow-950 shadow-lg border-2 border-white w-[60%] md:w-[30%] absolute right-[2.5rem] top-[4rem] duration-300 ease-in-out flex flex-col  justify-center items-center gap-[2rem] px-[2.5rem] py-[1rem] text-3xl overflow-hidden rounded-lg font-[font-2] transition-opacity"
             : "hidden transition-all ease-in-out duration-300"
         }
       >
-        <h1 onClick={() => scrollToSection("/")} className="cursor-pointer">
+        <h1
+          onClick={() => {
+            window.scrollTo(0, 0);
+            setMenuOpen(!menuOpen);
+          }}
+          className="cursor-pointer hover:text-gray-400  "
+        >
           Home
         </h1>
-        <h1 onClick={() => scrollToSection("about")} className="cursor-pointer">
+        <h1
+          onClick={() => scrollToSection("about")}
+          className="cursor-pointer hover:text-gray-400  "
+        >
           About
         </h1>
         <h1
           onClick={() => scrollToSection("projects")}
-          className="cursor-pointer"
+          className="cursor-pointer hover:text-gray-400  "
         >
           Projects
         </h1>
         <h1
           onClick={() => scrollToSection("connect")}
-          className="cursor-pointer"
+          className="cursor-pointer hover:text-gray-400  "
         >
           Contact
         </h1>
